7e4c28dba5baa9d8ca9d860340144be7603e9f0c
Benjamin Sykes <33205078+sykeben@users.noreply.github.com>
2018-05-16 09:26:03 -0400

    Updated PyCode for more demos.
    - Made counter only go up to ten.
    - Added bare bones of a system info demo.

--- pycode/main.py
+++ pycode/main.py
@@ -9,10 +9,19 @@ print "- \"sleep\" from \"time\""
 from time import sleep
 print ""
 
+print "There is no true kernel for PyOS yet,"
+print "so the following demos show that it works."
+print ""
+
 print "### COUNTING UP DEMO ###"
-print "There is no true kernel for PyOS yet, so this just shows that it works."
-n = 0
-while True:
+print "This will count from 0 to 10."
+for n in range(0, 10)
     n = n + 1
     print str(n)
-    sleep(1)
+    sleep(0.25)
+print ""
+
+print "### SYSTEM INFO DEMO ###"
+print "Not implimented yet!"
+print "Exiting in 3 seconds . . ."
+sleep(3)
